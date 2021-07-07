5566032aedc3b5271c35bcd64728c7d9e7b20b46
Muhammad Febrian Hasibuan <83405871+rian0502@users.noreply.github.com>
2021-07-07 14:16:59 +0700

    Update ParsingTOinfix.cpp
    Ubah benar

--- ParsingTOinfix.cpp
+++ ParsingTOinfix.cpp
@@ -1,61 +1,44 @@
-#include<iostream>
-#include<vector>
-#include<cstring>
+#include <iostream>
+#include  <string>
+#include <cstring>
 using namespace std;
 
-int main(int argc, char const *argv[]){
-    string copyStr;
-    string ubah = "-1*";
-    vector<string>pasteSTR;
-    getline(cin,copyStr);
-    for(int i=0 ; i < copyStr.length();i++){
-        if(copyStr[i] == ' '){
-            copyStr.erase(i,1);
+int main (){
+    string kata;
+    getline(cin,kata);    
+    cout<<"Print : ";
+	int N =  kata.length();
+    for(int x = 0; x < N ; x++){
+        if(kata[x] == '-' && (kata[x-1] == '+' || kata[x-1] == '-' ||kata[x-1] == '*' || kata[x-1] == '/'|| kata[x-1] == '%' )){
+            cout << "-1 * ";
         }
-        if((copyStr[i] == '-' and copyStr[i+1] == '(') and (copyStr[i-1] == '+' or 
-        copyStr[i-1] == '-' or copyStr[i-1] == '/' or copyStr[i-1] == '*' or copyStr[i-1] == '%')){
-            copyStr.replace(i,1,ubah);
+        else if (kata[x] == '-' && (kata[x-1] >= '0'|| kata[x-2] >= '0')){
+            cout << " " << kata[x] << " ";
+        } 
+        else if (kata[x] == '-' && kata[x+1] >= '0'){
+            cout << kata[x];
         }
-        else if(copyStr[i] == '-' and (copyStr[i-1] == '+' or copyStr[i-1] == '*' or copyStr[i-1] == '/' or copyStr[i-1] == '%' or copyStr[i-1] == '-') ){
-            copyStr.replace(i,1,ubah);
-        }    
-    }
-    if(copyStr[0] == '-' and copyStr[1] == '('){
-            copyStr.replace(0,1,ubah);
-    }
-    int j = 0;
-    while (true){
-       if(copyStr[j] == '-' and copyStr[j+1] == '1' and (copyStr[j-1] == '+' or copyStr[j-1] == '*' or copyStr[j+1] == '/' or copyStr[j-1] == '%' or copyStr[j-1] == ')' or copyStr[j-1] == '(')){
-            pasteSTR.push_back(copyStr.substr(j,2));
-            j+=1;
+        else if (kata[x-1] >= '0' && kata[x] == '-' ){
+            cout << " " << kata[x] << " ";
         }
-        else if(copyStr[j] == '-' and copyStr[j+1] == '1' and (copyStr[j+2] == '+' or copyStr[j+2] == '*' or copyStr[j+1] == '/' or copyStr[j+2] == '%' or copyStr[j+2] == ')' or copyStr[j+2] == '(')){
-            pasteSTR.push_back(copyStr.substr(j,2));
-            j+=1;
+        else if (kata[x] == '-' && kata[x+1] == '(' ){ 
+            cout << "-1 * ";
         }
-        else if((copyStr[j] >= 48 and copyStr[j] <= 57) and (copyStr[j+1] >= 48 and copyStr[j+1] <= 57)){
-            pasteSTR.push_back(copyStr.substr(j,2));
-            j+=1;
+        else if (kata[x] >= '0'){
+            cout << kata[x];
         }
-        else if((copyStr[j] >= 48 and copyStr[j] <= 57)and(copyStr[j+1] == '+' or copyStr[j+1] == '*' or copyStr[j+1] == '/' or copyStr[j+1] == '%' or copyStr[j+1] == ')' or copyStr[j+1] == '(')){
-            pasteSTR.push_back(copyStr.substr(j,1));
+        else if (kata[x] == '('){
+            cout << kata[x] << " ";
         }
-        else if((copyStr[j] == '+' or copyStr[j] == '*' or copyStr[j] == '/' or copyStr[j] == '%' or copyStr[j] == ')' or copyStr[j] == '(' or copyStr[j] == '-')){
-            pasteSTR.push_back(copyStr.substr(j,1));
+        else if (kata[x] == ')'){
+            cout << " " << kata[x];
         }
-        else{
-            pasteSTR.push_back(copyStr.substr(j,1));
+        else if(kata[x] == '-' && ( kata[x-1] == '*' || kata[x-1] == '+' || kata[x-1] == '-' || kata[x-1] == '/'|| kata[x-1] == '%' )){
+            cout << "-1 * ";
         }
-        j++;
-        if(copyStr[j] == copyStr[copyStr.length()]){
-            break;
+        else if (kata[x] == '-' || kata[x] == '+' || kata[x] == '*' || kata[x] == '/' || kata[x] == '%') {
+            cout << " " << kata[x] << " ";
         }
-    }
-
-    for(auto e:pasteSTR){
-        cout<<e<<" ";
-    }
-    cout<<endl;
-
+	}
     return 0;
 }
